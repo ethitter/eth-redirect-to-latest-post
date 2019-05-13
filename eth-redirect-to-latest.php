8eedeffc26021028c411df35ec853a3e348eb891
Erick Hitter <git-contrib@ethitter.com>
2019-05-12 17:14:29 -0700

    Introduce loader to bootstrap renamed plugin

---
+++ eth-redirect-to-latest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Load plugin.
+ *
+ * @package ETH_Redirect_To_Latest_Post.
+ */
+
+/**
+ * Plugin Name: ETH Redirect to Latest Post
+ * Plugin URI: https://ethitter.com/plugins/
+ * Description: Redirect a chosen slug to the whatever is currently the latest post
+ * Author: Erick Hitter
+ * Version: 0.2.2
+ * Author URI: https://ethitter.com/
+ * Text Domain: eth_redirect_to_latest_post
+ * Domain Path: /languages/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+require_once __DIR__ . '/inc/class-eth-redirect-to-latest.php';
--- inc/class-eth-redirect-to-latest.php
+++ inc/class-eth-redirect-to-latest.php
@@ -1,29 +1,13 @@
 <?php
-/*
-Plugin Name: ETH Redirect to Latest Post
-Plugin URI: https://ethitter.com/plugins/
-Description: Redirect a chosen slug to the whatever is currently the latest post
-Author: Erick Hitter
-Version: 0.2.2
-Author URI: https://ethitter.com/
-Text Domain: eth_redirect_to_latest_post
-Domain Path: /languages/
+/**
+ * Plugin functionality.
+ *
+ * @package ETH_Redirect_To_Latest_Post
+ */
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+/**
+ * Class ETH_Redirect_To_Latest_Post.
+ */
 class ETH_Redirect_To_Latest_Post {
 	/**
 	 * PLUGIN SETUP
