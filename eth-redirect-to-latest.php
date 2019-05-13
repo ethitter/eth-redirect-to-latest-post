7ac7ec8312bb6f6ffe76daed2917f816fe0eb906
Erick Hitter <git-contrib@ethitter.com>
2019-05-12 17:22:38 -0700

    PHPCS

--- eth-redirect-to-latest.php
+++ eth-redirect-to-latest.php
@@ -30,4 +30,4 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-require_once __DIR__ . '/inc/class-eth-redirect-to-latest.php';
+require_once __DIR__ . '/inc/class-eth-redirect-to-latest-post.php';
